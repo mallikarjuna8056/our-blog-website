f54672b13f4091abddc317cf214b7f403edc73cc
Malli Karjuna <kalavapallimallikarjunanaidu@gmail.com>
2026-06-27 20:52:08 +0530

    Initial commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+function Footer() {
+    return (
+        <div className='footer-div'>
+            <h1>Developed by Mallikarjuna Naidu</h1>
+        </div>
+    )
+}
+export default Footer;
